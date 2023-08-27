1b7c635ecd2655c29a7526bb3c5a86d005a8747a
manbodhvikram <manbodhvikram@gmail.com>
2023-08-27 15:00:10 +0530

    submitting code-Sun Aug 27 2023 15:00:06 GMT+0530 (India Standard Time)

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,34 +4,45 @@ import React,{useState} from "react";
 
 
 const Form=()=>{
-    let[user,setUser]=useState(
-        {name:"" , email:"" , password:""}
+    const[user,setUser]=useState(
+        {
+            name:"" , 
+            email:"" ,
+            password:"",
+        }
     );
 
-    let{name,email,password}=user
+//    let{name,email,password}=user
 
- const signup=(currentUser)=>{
-    // e.preventDefault();
-    // setUser();
-    console.log(currentUser);
-
+ function signup(e){
+    e.preventDefault();
+    console.log(user)
+  if(!name||!email|| !password){
+    alert("all fields are required")
+    setUser();
+    }
+    console.log(user);
    
- }
+}
+
+
  
 
     return(
         <div>
             <div>
-            <form onSubmit={signup(user)}>
-                <label htmlFor="name">Name </label>
-                <input type="text" onChange={(e)=>setUser({...user,name:e.target.value})}></input><br />
-                   <label htmlFor="name">Email</label>
-                <input type="text" onChange={(e)=>setUser({...user,email:e.target.value})}></input><br />
-                   <label htmlFor="name">Password </label>
-                <input type="text" onChange={(e)=>setUser({...user,password:e.target.value})}></input><br />
+            <form onSubmit={signup}>
+                <label name="userName">Name</label>
+                <input type="text" onChange={e=>setUser({...user,name:e.target.value})}></input><br />
+                   <label name="email">Email</label>
+                <input type="text" onChange={e=>setUser({...user,email:e.target.value})}></input><br />
+                   <label name="password">Password </label>
+                <input type="text" onChange={e=>setUser({...user,password:e.target.value})}></input><br />
+                <button type="submit">Submit</button>
             </form>
         </div>
-        <button type="submit">Submit</button>
+       
+        {/* <p>{user.map((users,index)=>console.log(users))}</p> */}
         </div>
     )
 }
